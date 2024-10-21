69916e97e682df114b046c0a006dd10cf6bae101
sergbelom <programmatistiss@gmail.com>
2024-10-21 20:27:58 +0300

    use default API_KEY

--- LinkPreviewApp/GlobalConfiguration.cs
+++ LinkPreviewApp/GlobalConfiguration.cs
@@ -4,7 +4,7 @@
     {
         public const string LINKPREVIEW_BASE = "api.linkpreview.net";
         public const string LINKPREVIEW_API_KEY_HEADER = "X-Linkpreview-Api-Key";
-        public const string LINKPREVIEW_API_KEY = "d94451fb5a9d9bef0d83f0fb910dded2";
+        public const string LINKPREVIEW_API_KEY = "123456";
         public const string LINKPREVIEW_FIELDS_QUERY_KEY = "fields";
         public const string LINKPREVIEW_FIELDS_QUERY_VALUE = "image_x,icon_type,locale";
         public const string LINKPREVIEW_LINK_QUERY_KEY = "q";
